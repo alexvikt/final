b8a8ddea0023a9c9de461eacc068c07823290a61
Яцюк Александр Викторович <yazuk@yazukvirtub.kraftway.lan>
2016-02-25 16:56:24 +0300

    Add getopt

--- final.cpp
+++ final.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-using namespace std;
 
-int main() {
-  cout << "Test final" << endl;
+int main(int argc, char **argv) {
+  int opt = 0;
+  char *ip;
+  int port;
+  char *dir;
+
+  printf("Start final\n");
+  opt = getopt( argc, argv, "h:p:d:");
+  while(opt != -1) {
+    switch(opt) {
+      case 'h':
+        ip = optarg;
+        break;
+
+      case 'p':
+        port = atoi(optarg);
+        break;
+
+      case 'd':
+        dir = optarg;
+        break;
+
+      default:
+        break;
+    }
+    opt = getopt( argc, argv, "h:p:d:");
+  }
+  printf("IP = %s  %d\n", ip, strlen(ip));
+  printf("Port = %d\n", port);
+  printf("Dir = %s\n", dir);
 return 0;
 }
