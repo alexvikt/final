abe9da7423d2dbcad4a988701be33c0d4377d7c1
Яцюк Александр Викторович <yazuk@yazukvirtub.kraftway.lan>
2016-02-26 10:44:35 +0300

    Add debug.log

--- final.cpp
+++ final.cpp
@@ -1,10 +1,18 @@
+#define DEBUG
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 
 int main(int argc, char **argv) {
+
+#ifdef DEBUG
+char logbuf[256] = "echo \0";
+system("echo Start... > debug.log");
+#endif
+  pid_t pid;
   int opt = 0;
   char *ip;
   int port;
@@ -31,8 +39,31 @@ int main(int argc, char **argv) {
     }
     opt = getopt( argc, argv, "h:p:d:");
   }
-  printf("IP = %s  %d\n", ip, strlen(ip));
-  printf("Port = %d\n", port);
-  printf("Dir = %s\n", dir);
+#ifdef DEBUG
+system("echo Init... >> debug.log");
+sprintf(&logbuf[5],"IP = %s Port = %d Dir = %s >> debug.log", ip, port, dir);
+system(logbuf);
+#endif
+  pid = fork();
+  if (pid < 0) {
+    printf("Error demonization!");  
+    exit(EXIT_FAILURE);
+  }
+  if (pid > 0) {
+#ifdef DEBUG
+system("echo End parent. >> debug.log");
+#endif
+    exit(EXIT_SUCCESS);
+  }
+
+#ifdef DEBUG
+system("echo In child. Close std. >> debug.log");
+#endif
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  while (1);
+
 return 0;
 }
