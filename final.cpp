6d90cd7f61173fd9103550a7ad94ec2e89cbe10e
Яцюк Александр Викторович <yazuk@yazukvirtub.kraftway.lan>
2016-02-26 16:53:20 +0300

    thread echo

--- final.cpp
+++ final.cpp
@@ -1,24 +1,70 @@
 #define DEBUG
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <pthread.h>
+
+#ifdef DEBUG
+char logbuf[256] = "echo \0";
+#endif
+
+struct inthr {
+int s;
+struct sockaddr_in a;
+};
+
+void *connect(void *arg) {
+#ifdef DEBUG
+system("echo Connect... >>  debug.log");
+#endif
+  inthr sock = *(inthr *) arg;
+  free ((inthr *) arg);
+  char buf[512];
+
+  for (;;) {
+    memset(buf,0, sizeof(char)*512);
+    int n = recv(sock.s, buf, 511, 0);
+    if (n < 0) {
+        if (errno != EINTR) {
+        exit(EXIT_FAILURE);
+      } 
+    }
+#ifdef DEBUG
+sprintf(&logbuf[5]," %d bytes received. >> debug.log", n);
+system(logbuf);
+#endif
+    sendto(sock.s, buf, strlen(buf), 0, (struct sockaddr *)& sock.a, sizeof(sock.a));
+  if (buf[0] == '!') break;
+  }
+  close (sock.s);
+#ifdef DEBUG
+system("echo Disconnect. Close socket. >>  debug.log");
+#endif
+}
 
 
 int main(int argc, char **argv) {
 
 #ifdef DEBUG
-char logbuf[256] = "echo \0";
 system("echo Start... > debug.log");
 #endif
   pid_t pid;
+  int socketfd;
+  inthr *pinthr;
+  struct sockaddr_in addr;
   int opt = 0;
   char *ip;
   int port;
   char *dir;
+  int thrres;
+  pthread_t a_thread;
 
-  printf("Start final\n");
   opt = getopt( argc, argv, "h:p:d:");
   while(opt != -1) {
     switch(opt) {
@@ -62,8 +108,46 @@ system("echo In child. Close std. >> debug.log");
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
   close(STDERR_FILENO);
-
-  while (1);
+  
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = inet_addr(ip);
+  addr.sin_port = htons(port);
+  if (bind(socketfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+#ifdef DEBUG
+system("echo Error calling bind. >>  debug.log");
+#endif
+    exit(EXIT_FAILURE);
+  }
+  if (listen(socketfd, 5)) {
+#ifdef DEBUG
+system("echo Error calling listen. >>  debug.log");
+#endif
+    exit(EXIT_FAILURE);
+  }
+  while (1) {
+#ifdef DEBUG
+system("echo Wait accept... >>  debug.log");
+#endif
+    int s = accept(socketfd, NULL, NULL);
+    if (s<0) {
+#ifdef DEBUG
+system("echo Error calling accept. >>  debug.log");
+#endif
+      exit(EXIT_FAILURE);
+    }
+#ifdef DEBUG
+system("echo Accept OK. Call connect. >>  debug.log");
+#endif
+    pinthr = (inthr *) malloc (sizeof(inthr));
+    pinthr->s = s;
+    pinthr->a = addr;
+    thrres = pthread_create(&a_thread, NULL, connect,(void *)pinthr);
+    if (thrres != 0) {
+      exit(EXIT_FAILURE);
+    }
+  }
 
 return 0;
 }
+
