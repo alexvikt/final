2cef2f4c57faec913a4cb133b4b35d0963e501a6
Alexander <alexvikt@yandex.ru>
2016-02-28 01:01:00 +0300

    Final

--- final.cpp
+++ final.cpp
@@ -1,5 +1,6 @@
 #define DEBUG
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
@@ -17,6 +18,7 @@ char logbuf[256] = "echo \0";
 struct inthr {
 int s;
 struct sockaddr_in a;
+char *d;
 };
 
 void *connect(void *arg) {
@@ -25,10 +27,17 @@ system("echo Connect... >>  debug.log");
 #endif
   inthr sock = *(inthr *) arg;
   free ((inthr *) arg);
+  bool notfound;
   char buf[512];
+  char method[32];
+  char uri[128];
+  char *ptr;
+  struct stat file_stat;
 
-  for (;;) {
     memset(buf,0, sizeof(char)*512);
+    memset(method,0, sizeof(char)*32);
+    memset(uri,0, sizeof(char)*128);
+
     int n = recv(sock.s, buf, 511, 0);
     if (n < 0) {
         if (errno != EINTR) {
@@ -39,9 +48,52 @@ system("echo Connect... >>  debug.log");
 sprintf(&logbuf[5]," %d bytes received. >> debug.log", n);
 system(logbuf);
 #endif
-    sendto(sock.s, buf, strlen(buf), 0, (struct sockaddr *)& sock.a, sizeof(sock.a));
-  if (buf[0] == '!') break;
-  }
+    if (n > 0) {
+      sscanf(buf,"%s %s %*s",method,uri);
+#ifdef DEBUG
+sprintf(&logbuf[5],"Method:  %s. URI: %s  >> debug.log", method, uri);
+system(logbuf);
+#endif
+      if (strlen(method)==3 && strcmp(method,"GET")==0) {
+#ifdef DEBUG
+system("echo Method GET - OK. >>  debug.log");
+#endif
+      ptr = strchr(uri,'?');
+      if (ptr != NULL) *ptr = '\0';
+      strcpy(method,uri);
+      strcpy(uri,sock.d);
+      strcat(uri,method);
+      notfound = true;
+      if (stat(uri, &file_stat) == 0) {
+#ifdef DEBUG
+sprintf(&logbuf[5],"File %s exist. %lu byte.  >> debug.log", uri, file_stat.st_size);
+system(logbuf);
+#endif
+      int fd;
+      fd = open(uri, O_RDONLY);
+      if (fd != -1) {
+        notfound = false;
+        char *buffer = (char *) malloc(file_stat.st_size + 100); 
+        memset(buffer,0, sizeof(char) * (file_stat.st_size + 100));
+        sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-length: %lu\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n", file_stat.st_size);
+        read (fd,&buffer[strlen(buffer)],file_stat.st_size);
+        close (fd);
+        sendto(sock.s, buffer, strlen(buffer), 0, (struct sockaddr *)& sock.a, sizeof(sock.a));
+        free(buffer);
+      }
+      } 
+      if (notfound) {
+#ifdef DEBUG
+sprintf(&logbuf[5],"File %s not exist.  >> debug.log", uri);
+system(logbuf);
+#endif
+      memset(buf,0, sizeof(char)*512);
+      sprintf(buf,"HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n");
+      sendto(sock.s, buf, strlen(buf), 0, (struct sockaddr *)& sock.a, sizeof(sock.a));
+      }
+      
+      }
+    }
   close (sock.s);
 #ifdef DEBUG
 system("echo Disconnect. Close socket. >>  debug.log");
@@ -142,6 +194,7 @@ system("echo Accept OK. Call connect. >>  debug.log");
     pinthr = (inthr *) malloc (sizeof(inthr));
     pinthr->s = s;
     pinthr->a = addr;
+    pinthr->d = dir;
     thrres = pthread_create(&a_thread, NULL, connect,(void *)pinthr);
     if (thrres != 0) {
       exit(EXIT_FAILURE);
